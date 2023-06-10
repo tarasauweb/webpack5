2acd3bf2cc70c31170a5203bbfdbfcd96b43f4ca
Artur Tarasau <tarasov.a.web@gmail.com>
2023-06-10 10:39:19 +0300

    fix:entry

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/main.js',
+    entry: './src/main.ts',
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js',
